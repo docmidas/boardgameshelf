64c9947ab5dabc36bc6d8d1832ed14cb26da421e
dex <dexterrgolden@gmail.com>
2016-08-03 11:11:03 -0500

    wed morn

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,25 @@
 class HomeController < ApplicationController
 
-
-
   get '/login/?' do
-    @games_liked = Game.where user_id: session[:user_id]
+    # user = User.find_by username: params["username"]
+      if session[:is_logged_in]
+        redirect to('/../profile')        
+      else  
+        erb :login
+      end  
+    
+    
     erb :login    
   end
 
   get '/profile/?' do
     @games_liked = Game.where user_id: session[:user_id]
-    erb :profile    
+    user = User.find(session[:user_id])
+      if user
+        @username = user.username
+      end  
+     erb :profile  
+     
   end
 
 
@@ -17,28 +27,17 @@ class HomeController < ApplicationController
     user = User.find_by username: params["username"]
       if user 
         compare_to = BCrypt::Password.new(user.password)
-        if compare_to == params["password"]
-          @username = params["username"]
-          @password = user["password"]
-          @email = user["email"]
-          
+        if compare_to == params["password"]          
           session[:is_logged_in] = true
           session[:user_id] = user.id
-
-          puts "isLoggedIn: #{session[:is_logged_in]}"
-
-          @games_liked = Game.where user_id: session[:user_id]
-          # {status: "Okay", message: "Found USER! #{user.username}"}.to_json
-          erb :profile
+          # puts "isLoggedIn: #{session[:is_logged_in]}"
+          redirect to('/profile') 
         else
           {status: "ERROR", message: "right user, WRONG password"}.to_json         
-        end
-        
-        
+        end        
       else
         {status: "ERROR", message: "Could NOT find user"}.to_json
-      end 
-    
+      end    
   end
 
   get '/' do      
