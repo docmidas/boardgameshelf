8b9264035ad9e1d14b49aea1116e03fd7a418764
dex <dexterrgolden@gmail.com>
2016-08-30 12:21:41 -0500

    added prof

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
 
   enable :sessions
 
-	set :database, {adapter: 'postgresql', database: 'postgres://ctgjjgdwzouwwo:HD1o5NCQPVMFo1kCcmL3ax9yu6@ec2-23-23-76-90.compute-1.amazonaws.com:5432/d70711oclki99p' }  
+	set :database, {adapter: 'sqlite3', database: File.dirname(__FILE__) + '/../../db.sqlite3' }  
 	set :public_folder, File.dirname(__FILE__) + '/../public'
 	set :views, File.dirname(__FILE__) + '/../views'
 end	
